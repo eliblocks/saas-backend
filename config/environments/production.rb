a88c2c959f999d5b1222e998f00b2c2c3ef2771a
Eli Block <18074026+eliblocks@users.noreply.github.com>
2020-08-26 11:32:13 -0400

    fix devise mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,7 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { host: 'https://generic-saas.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'https://nostalgic-tesla-951e56.netlify.app' }
 
   ActionMailer::Base.smtp_settings = {
     user_name: 'apikey',
