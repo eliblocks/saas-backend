efcae32e5d219445c24b074adba90840e4de6745
Eli Block <18074026+eliblocks@users.noreply.github.com>
2021-07-11 09:33:21 -0400

    fix sendgrid credentials

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,11 +58,11 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { host: 'https://nostalgic-tesla-951e56.netlify.app' }
+  config.action_mailer.default_url_options = { host: 'https://genericsaas.com' }
 
   ActionMailer::Base.smtp_settings = {
-    user_name: 'genericsaas',
-    password: Rails.application.credentials.sendgrid_secret,
+    user_name: 'apikey',
+    password: Rails.application.credentials.sendgrid_key,
     domain: 'heroku.com',
     address: 'smtp.sendgrid.net',
     port: 587,
