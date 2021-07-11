091129e948a6bf36284cf4bd4b7703f35faf59c7
Eli Block <18074026+eliblocks@users.noreply.github.com>
2021-07-11 14:04:20 -0400

    add dashboard data

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,13 @@
 class DashboardController < ApplicationController
+	include ActionView::Helpers::DateHelper
+
 	before_action :check_user_authentication!
 
 	def index
-		render json: { message: "You are authenticated!" }
+		render json: {
+			account_age: time_ago_in_words(current_account.created_at),
+			users_count: current_account.users.count,
+			plan: current_account.plan,
+		}
 	end
 end
